e481b241c88ee4e5d9531a39453cfd7a89cf20f6
Reece Dudley <ll16rdjd@leeds.ac.uk>
2018-12-04 22:00:33 +0000

    Emotion entries are displayed now. Form has been added to update the emotion field of entries. Emotion field in entries can be updated.

--- app/views.py
+++ app/views.py
@@ -1,34 +1,37 @@
 from flask import render_template, flash
 from app import app
-from .forms import EntryForm
+from .forms import EntryForm, UpdateForm
 
-# default route for viewing all tasks
-@app.route('/')
+# default route for viewing all entries
+@app.route('/', methods=['GET', 'POST'])
 def base():
-    # entires = getEntries(2)
+    entries = getEntries()
+    form = UpdateForm()
 
     # form data is only submitted if it is valid
-    # if form.validate_on_submit():
+    if form.validate_on_submit():
         # try block tests if the task has been marked as finished and if it has a success message is
         # displayed and if not, an error message is displayed
-        # try:
-            # completeTask(form.number.data)
-            # flash('The task has been marked as finished.')
-        # except:
-            # flash("An error occurred and the task was not marked as finished.")
+        try:
+            updateEntry(form.number.data, form.emotion.data)
+            flash('The entry has been updated.')
+        except:
+            flash('An error occurred and the entry was not updated.')
 
-    # renders the 'all.html' page instead of 'base.html' to incorporate the features needed for
-    # displaying tasks and marking tasks as finished
-    return render_template('entries.html')
+    # renders the 'entries.html' page instead of 'base.html' to incorporate the features needed for
+    # displaying and updating entries
+    return render_template('entries.html',
+                           entries=entries,
+                           form=form)
 
-# route for viewing incomplete tasks
+# route for creating a new entry
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     form = EntryForm()
 
     # form data is only submitted if it is valid
     if form.validate_on_submit():
-        # try block tests if the task has been marked as finished and if it has a success message is
+        # try block tries to add the entry to the database and if it is successful a  message is
         # displayed and if not, an error message is displayed
         try:
             submitEntry(form.emotion.data)
@@ -39,10 +42,23 @@ def create():
     return render_template('create.html',
                            form=form)
 
+# this function retrieves the emotion entries from the database
+def getEntries():
+    from app import db, models
+
+    return models.Entry.query.all()
+
+# this function updates the value of the specified emotion entry
+def updateEntry(number, emotion):
+    from app import db, models
+
+    entry = models.Entry.query.get(number)
+    entry.emotion = emotion
+    db.session.commit()
+
 # this function adds the data about the new emotion entry to the database
 def submitEntry(emotion):
     from app import db, models
-    import datetime
 
     newEntry = models.Entry(emotion=emotion)
     db.session.add(newEntry)
